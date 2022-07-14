3690ef3b06fd7ea3346cb80fc7fdf2c16fe0c469
soongonkim <soongon@hucloud.co.kr>
2022-07-14 12:27:29 +0900

    initial commit

--- dbutils/connect.js
+++ dbutils/connect.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 
 const uri = 'mongodb+srv://admin:admin@cluster1.2bvmrhi.mongodb.net/?retryWrites=true&w=majority';
+// 주석
+
+/*
+멀티라인 주석
+입니다.
+*/
+
 
 const connectToMongo = async () => {
   try {
