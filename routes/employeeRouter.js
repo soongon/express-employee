a5a361b6156c52d200234e43bbae12d56c1c67d8
soongonkim <soongon@hucloud.co.kr>
2022-07-14 14:05:29 +0900

    initial commit

--- routes/employeeRouter.js
+++ routes/employeeRouter.js
@@ -1,44 +1,33 @@
 const express = require('express');
+const Employee = require('../models/Employee');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // 1. 직원 데이터가 있는 몽고디비에서 직원 데이터를 가져온다.
   // 2. 직원데이터를 응답한다.
   //    json 데이터를 응답한다.
-  res.json([
-    {
-      id: 1,
-      name: 'kim',
-      addr: {
-        cidy: 'seoul',
-        detail: '종로구'
-      },
-      position: '과장'
-    },
-    {
-      id: 2,
-      name: 'lee',
-      addr: 'seoul',
-      position: '부장'
-    },
-    {
-      id: 3,
-      name: 'park',
-      addr: 'seoul',
-      position: '차장'
-    }
-  ]);
+  try {
+    const employees = await Employee.find({});
+    res.json(employees);
+  } catch (err) {
+    res.json(err);
+  }
 });
 router.get('/:empId', (req, res) => {
   // empId 를 확보해서 몽고디비에 조회한 후 데이터를 반환
+  //Employee.findOne({id: req.params.empId})
   console.log(req.params.empId);
   res.json({empId: req.params.empId});
 });
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // 1. 직원데이터를 클라이언트에서 전달해주면..
   // 2. 그 데이터를 받아서 몽고디비에 인서트..
-  console.log(typeof req.body);
-  res.json(req.body);
+  try {
+    const saved = await Employee.create(req.body);
+    res.json(saved);
+  } catch (err) {
+    res.json(err);
+  }
 });
 router.put('/', (req, res) => {
   res.json('직원수정..');
